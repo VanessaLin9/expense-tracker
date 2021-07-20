eb004f0e75c6c2b7dcf414b3d9f287e6a1090d62
VanessaLin9 <vanessa7591@gmail.com>
2021-07-21 06:07:57 +0800

    feat: add 'config' configure

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expense'
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongoDB error!')
+})
+
+db.once('open', () => {
+  console.log('mongoDB connected!')
+})
+
+module.exports = db
